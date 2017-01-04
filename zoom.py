710b4421d6d8ffa95399a34460327d89114f0ee7
xiongchiamiov <xiong.chiamiov@gmail.com>
2017-01-04 15:30:12 -0800

    Add benediction

--- zoom.py
+++ zoom.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+# May you recognize your weaknesses and share your strengths.
+# May you share freely, never taking more than you give.
+# May you find love and love everyone you find.
+
 import os
 import sqlite3
 
