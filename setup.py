288df51ad2034a388b8c15543980537280c94d76
xiongchiamiov <xiong.chiamiov@gmail.com>
2017-01-08 10:50:08 -0800

    Add classifiers for PyPI

--- setup.py
+++ setup.py
@@ -17,4 +17,18 @@ setup(
         'zoom_shortener': ['templates/*', 'schema.sql'],
     },
     scripts=['bin/zoom'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
